39f5b3f387bf5963cd1eb6294cac20b4d89976ca
Mark Brown <mark.brown@unosquare.com>
2024-01-18 10:34:24 +0000

    Added thresholds

--- test/load/script.js
+++ test/load/script.js
@@ -4,6 +4,10 @@ import { group, check } from "k6";
 export const options = {
   vus: 10,
   duration: "30s",
+  thresholds: {
+    http_req_failed: ['rate<0.01'],
+    http_req_duration: ['p(95)<200'], 
+  },
 };
 
 export default function () {
